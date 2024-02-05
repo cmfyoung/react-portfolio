3add23566ad8523a96a3d1e32b596bc381f2d7ac
cmfyoung <cmfyoung7@gmail.com>
2024-02-04 21:04:26 -0500

    added resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./App.css"
+
 const App = () => {
     return (
         <div className="wrapper">
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,11 +1,13 @@
 export default function Resume() {
-    return (
-      <div>
-        <h1>Resume</h1>
-        <p>
-          I have a particular set of skills
-        </p>
-      </div>
-    );
-  }
+  const pdfUrl = 'https://drive.google.com/file/d/1E5tWYIRQAgMYc8yzrxTFMcQzh2fxjScd/view?usp=sharing';  
   
+  return (
+      <div>
+        <h1>Resume</h1> 
+        <a href={pdfUrl}>
+        View Resume (PDF)
+      </a>
+    </div>
+  );
+};
+
